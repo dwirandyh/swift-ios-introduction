b260f6bb7d086cac1525491c2ac3ab0cd51cc5f5
Dwi Randy H <dwirandyherdinanto@gmail.com>
2020-01-27 22:18:01 +0700

    Switch statement

---
+++ swift/switch.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+var age = 15
+
+switch age {
+    case 14:
+        print("You are 14 years old")
+    case 15:
+        print("You are 15 years old")
+    default:
+        print("Im not sure how old are you")
+}
