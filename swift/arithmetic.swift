ef103e79526a11317099b5c5cebe812c4acb381c
Dwi Randy H <dwirandyherdinanto@gmail.com>
2020-01-27 16:38:52 +0700

    Aritmathic

---
+++ swift/arithmetic.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+var num1 = 10
+var num2 = 3
+
+let addition =  num1 + num2
+print(addition)
+
+let substraction = num1 - num2
+print(substraction)
+
+let multiplication = num1 * num2
+print(multiplication)
+
+let division = num1 / num2
+print(division)
+
+let modulus = num1 % num2
+print(modulus)
+
+num1 += 2
+print(num1)
+num1 -= 2
+print(num1)
+
+var sent = "Hy my name is 'Randy'"
+print(sent)
---
+++ swift/constant.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let age = 16
+print(age)
+
+let message = "Hello"
+print(message)
+
+var (firstName, middleName, lastName) = ("Dwi", "Randy", "Herdinanto")
+print(firstName)
+print(middleName)
+print(lastName)
