313892145fa960e5c03c26f86f368165cabe1dbe
Dwi Randy H <dwirandyherdinanto@gmail.com>
2020-01-27 16:50:29 +0700

    Conditional Statement

---
+++ swift/conditional_statement.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var isRainy = false
+var age = 15
+
+if age == 15 {
+    print("You are 15")
+}else if age == 16 {
+    print("You are 16")
+} else {
+    print("Hi there")
+}
+
+if (age == 15 || age < 17){
+    print("You are amazing")
+}
